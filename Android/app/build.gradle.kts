a66d93d08cf9cf2607735f421927d19de9a141db
wouter <thys_wouter@hotmail.com>
2025-05-05 09:50:16 +0200

    Get it working again with updated versions

--- Android/app/build.gradle.kts
+++ Android/app/build.gradle.kts
@@ -11,11 +11,11 @@ plugins {
 
 android {
     namespace = "com.cacti.cactiphone"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.cacti.cactiphone"
-        minSdk = 33
+        minSdk = 30
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -93,13 +93,13 @@ protobuf {
 
 dependencies {
 
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.core:core-ktx:1.16.0")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
     implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.6")
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
 
     implementation("androidx.preference:preference-ktx:1.2.1")
@@ -148,14 +148,14 @@ dependencies {
     implementation("com.google.protobuf:protobuf-kotlin-lite:$protobufVersion")
 //
     // Room
-    val androidxRoomVersion = "2.6.1"
+    val androidxRoomVersion = "2.7.1"
     implementation("androidx.room:room-runtime:$androidxRoomVersion")
     implementation("androidx.room:room-ktx:$androidxRoomVersion")
     annotationProcessor("androidx.room:room-compiler:$androidxRoomVersion")
     kapt("androidx.room:room-compiler:$androidxRoomVersion")
 
     //Hilt
-    val daggerHiltVersion = "2.49"
+    val daggerHiltVersion = "2.51.1"
     val androidxHiltVersion = "1.2.0"
     implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
     kapt("com.google.dagger:hilt-android-compiler:$daggerHiltVersion")
@@ -168,11 +168,11 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:$navVersion")
 
     // Scanning
-    val scanningVersion = "17.2.0"
+    val scanningVersion = "17.3.0"
     implementation("com.google.mlkit:barcode-scanning:$scanningVersion")
     implementation("com.google.android.gms:play-services-mlkit-barcode-scanning:18.3.0")
 
-    val cameraxVersion = "1.3.2"
+    val cameraxVersion = "1.4.2"
     implementation("androidx.camera:camera-core:${cameraxVersion}")
     implementation("androidx.camera:camera-camera2:${cameraxVersion}")
     implementation("androidx.camera:camera-lifecycle:${cameraxVersion}")
